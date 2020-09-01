bbd8b114630de019fbd37718275bc4d9992d130e
Enes52001 <60393441+Enes52001@users.noreply.github.com>
2020-09-01 13:00:56 +0200

    main geupdate

--- src/main.java
+++ src/main.java
@@ -3,14 +3,14 @@ import java.sql.*;
 public class main {
     public static void main(String[] args) {
         try{
-            Connection con = DriverManager.getConnection( "jdbc:postgresql:://[127.0.0.1]:55255/", "postgres", "wachtwoord");
+            Connection con = DriverManager.getConnection( "jdbc:postgresql:://[127.0.0.1]:55255/ovchip", "postgres", "wachtwoord");
 
             Statement stm = con.createStatement();
 
-            ResultSet rs = stm.executeQuery("tekst");
+            ResultSet rs = stm.executeQuery("select * from reizigers");
 
             while (rs.next()){
-                System.out.println(rs.getString("naam"));
+                System.out.println(rs.getString("id = "+rs.getString("reiziger_id")));
             }
         }catch(Exception e){
             e.getStackTrace();
