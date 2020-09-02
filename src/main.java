5a31c872cf0c1e32e1b1b52136a5e6a2bf2e7641
Enes52001 <60393441+Enes52001@users.noreply.github.com>
2020-09-02 14:43:16 +0200

    p1 af

--- src/main.java
+++ src/main.java
@@ -3,17 +3,22 @@ import java.sql.*;
 public class main {
     public static void main(String[] args) {
         try{
-            Connection con = DriverManager.getConnection( "jdbc:postgresql:://localhost/ovchip", "postgres", "wachtwoord");
+            Connection con = DriverManager.getConnection( "jdbc:postgresql://localhost/ovchip?user=postgres&password=wachtwoord");
 
             Statement stm = con.createStatement();
 
-            ResultSet rs = stm.executeQuery("select * from reizigers");
-
+            ResultSet rs = stm.executeQuery("select * from reiziger");
+            System.out.println("Alle reizigers:\n");
             while (rs.next()){
-                System.out.println(rs.getString("id = "+rs.getString("reiziger_id")));
+                System.out.println("   #"+rs.getString("reiziger_id")+": "
+                        +rs.getString("voorletters")
+                        +rs.getString("achternaam")
+                        +rs.getString("tussenvoegsel")+
+                        " ("+rs.getString("geboortedatum")+")"
+                );
             }
-        }catch(Exception e){
-            e.getStackTrace();
+        }catch(SQLException e){
+            System.err.println("er is een fout opgetreden: "+e.getMessage());
         }
     }
 }
